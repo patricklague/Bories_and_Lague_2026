68632aa6d5cba0f05170ff376c9cde17fc2fbdf3
sbories <soeurgrimm@gmail.com>
2026-04-10 11:39:07 -0400

    update density_profile

--- figures/data/SUPP_membrane_parm/densityProfiles/get_density.py
+++ figures/data/SUPP_membrane_parm/densityProfiles/get_density.py
@@ -4,32 +4,41 @@ import numpy as np
 import glob
 from functools import reduce
 
-# Lire les fichiers trajectory*.dat
-file_list = sorted(glob.glob("trajectory*.dat"))
+# Lire les fichiers trajectory{traj}-{bloc}.dat
+file_list = sorted(glob.glob("trajectory*-*.dat"))
 print("Fichiers trouvés :", file_list)
 
 # Liste pour stocker les DataFrames transformés
 dfs = []
+col_names = []
 
-for i, filename in enumerate(file_list, start=1):
+for filename in file_list:
+    # Extraire traj et bloc du nom: trajectory{traj}-{bloc}.dat
+    base = filename.replace('.dat', '').replace('trajectory', '')
+    parts = base.split('-')
+    traj = parts[0]
+    bloc = parts[1]
+
+    col_name = f"dens_traj{traj}_bloc{bloc}"
+    col_names.append(col_name)
+
     # Lecture du fichier (pas d'en‑tête, séparation par espaces)
     df = pd.read_csv(filename, sep=r'\s+', header=None)
-    
+
     # Ne garder que la colonne z (colonne 0) et la densité (dernière colonne)
     df_tmp = df[[0, df.shape[1] - 1]].copy()
-    df_tmp.columns = ['z', f'dens_{i}']
-    
-    # Stocker dans la liste
+    df_tmp.columns = ['z', col_name]
+
     dfs.append(df_tmp)
 
 # Fusionner tous les DataFrames sur la colonne 'z'
 merged = reduce(lambda left, right: pd.merge(left, right, on='z'), dfs)
 
-# Calcul de la densité moyenne et de l'erreur-type (SE)
-n = len(dfs)
-dens_cols = [f'dens_{i}' for i in range(1, n+1)]
-merged['dens_moy'] = merged[dens_cols].mean(axis=1)
-merged['se_moy']   = merged[dens_cols].std(axis=1) / np.sqrt(n)
+# Calcul de la densité moyenne et de l'erreur-type (SE) sur les 9 blocs
+dens_cols = [col for col in merged.columns if col.startswith("dens_")]
+n = len(dens_cols)
+merged['dens_mean'] = merged[dens_cols].mean(axis=1)
+merged['se']   = merged[dens_cols].std(axis=1, ddof=1) / np.sqrt(n)
 
 # Sauvegarde du résultat
 output_filename = "trajectory-dens.dat"
--- figures/data/SUPP_membrane_parm/densityProfiles/run.sh
+++ figures/data/SUPP_membrane_parm/densityProfiles/run.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-#FAITS: "SCYM" "SCY" "SCI" "SCL" "SCF" "SCS" "SCT" "SCKN" "SCRN" "SCDN" "SCEN" "SCQ" "SCN" "SCHE" "SCHD" "SCC" "SCM"
-#A FAIRE: 
+#FAITS: "SCY" "SCI" "SCL" "SCF" "SCS" "SCT" "SCKN" "SCRN" "SCDN" "SCEN" "SCQ" "SCN" "SCHE" "SCHD" "SCC" "SCM"
+#A FAIRE:
 #(-N): "SCHP" "SCK" "SCR" "SCCM" "SCD" "SCE"
 #traj4:"SCV" "GLYD" "SCA" "SCP" "SCW"
+#traj456: "SCYM"
+#(-1):"SCRN" "SCW"
 
-aafile=( "SCYM" )
+aafile=("SCRN" "SCW")
 profiles=("chains" "choline" "phosphate" "total" "water")
-traj=(4 5 6)
+traj=(1 2 3)
 
 for i in "${profiles[@]}"
 do
@@ -15,17 +17,25 @@ do
   do
     for t in "${traj[@]}"
     do
-      cp /media/bories/Backup/bories/Documents/Travail/results/homoPOPC-aa/homoPOPC-$aa/analyses/traj$t/data/densityProfiles/profile-${i}600.dat ./trajectory$t.dat
-      if [ $t = 4 ]; then
-        mv trajectory$t.dat trajectory1.dat
-      elif [ $t = 5 ]; then
-        mv trajectory$t.dat trajectory2.dat
-      elif [ $t = 6 ]; then
-        mv trajectory$t.dat trajectory3.dat
-      fi
+      cp /media/bories/Backup/bories/Documents/Travail/results/homoPOPC-aa/homoPOPC-$aa-1/analyses/traj$t/data/densityProfiles/profile-${i}[3-5].dat .
+      for j in 3 4 5
+      do
+        mv profile-${i}$j.dat trajectory$t-$((j-2)).dat
+
+        if [ $t = 4 ]; then
+          mv trajectory$t-$((j-2)).dat trajectory1-$((j-2)).dat
+        elif [ $t = 5 ]; then
+          mv trajectory$t-$((j-2)).dat trajectory2-$((j-2)).dat
+        elif [ $t = 6 ]; then
+          mv trajectory$t-$((j-2)).dat trajectory3-$((j-2)).dat
+        fi
+      done
     done
     python get_density.py
-    mv trajectory-dens.dat ${aa,,}-$i.dat
+    mv trajectory-dens.dat ${aa,,}-1-$i.dat
+    if [ $aa = "NONE" ]; then
+      mv ${aa,,}-$i.dat popc-$i.dat
+    fi
     rm trajectory*.dat
   done
 done
